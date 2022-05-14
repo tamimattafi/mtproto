074e6e5d42642d16ef04653b3bd9e47626bbd84b
Tamim Attafi <tamimattafi@Tamims-MacBook-Pro.local>
2022-05-15 02:06:45 +0700

    refactor: add separate module for generator

--- generator/settings.gradle.kts
+++ generator/settings.gradle.kts
@@ -6,13 +6,10 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
     }
 }
-rootProject.name = "MTProto"
-include ':sample'
-include ':client'
-include ':server'
+rootProject.name = "mtproto.generator"
